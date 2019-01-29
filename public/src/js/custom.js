7a5dbcba2310d53016edae3a35c01325c9ad61a2
Jake Bown <jakebown@gmail.com>
2019-01-29 08:23:07 +0000

    Update custom.js

--- public/src/js/custom.js
+++ public/src/js/custom.js
@@ -1,3 +1,13 @@
 (function($) {
 
-}(jQuery);
+  $.ajax({
+    type: 'GET',
+    url: 'https://wakatime.com/share/@540f0dd6-7f2d-46d6-817d-0fe1ca620988/49b793ff-5d41-4a54-8c7c-614a32f80008.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      console.log(response.data);
+    },
+  });
+    
+  
+})(jQuery);
