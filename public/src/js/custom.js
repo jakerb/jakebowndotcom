f05688fa666beb8dfbd5080fbad63944b6461b87
Jake Bown <jakebown@gmail.com>
2019-01-29 08:29:25 +0000

    Update custom.js

--- public/src/js/custom.js
+++ public/src/js/custom.js
@@ -5,7 +5,18 @@
     url: 'https://wakatime.com/share/@540f0dd6-7f2d-46d6-817d-0fe1ca620988/49b793ff-5d41-4a54-8c7c-614a32f80008.json',
     dataType: 'jsonp',
     success: function(response) {
-      console.log(response.data);
+      if(typeof response.data == 'object') {
+        var target = 'uk-activity-p';
+        var target_div = 'uk-activity';
+        var str = 'Over the past 7 days I have used ';
+        for (var i = response.data.length - 1; i >= 0; i--) {
+          var data = response.data[i];
+          str += (!i ? ' and ' : '') + data.name + " " + data.percent + "% ";
+        }
+      }
+      
+      $(target).text(str);
+      $(target_div).removeClass('uk-hidden');
     },
   });
     
